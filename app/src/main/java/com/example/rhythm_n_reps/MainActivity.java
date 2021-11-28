aae7fe215e2c2738fd2b44701a3b7b0ccf6e7986
jeannillehici <hicianoj@northeastern.edu>
2021-11-27 22:33:02 -0500

    test

--- app/src/main/java/com/example/rhythm_n_reps/MainActivity.java
+++ app/src/main/java/com/example/rhythm_n_reps/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.rhythm_n_reps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.workshopButton:
+                Intent workshopIntent = new Intent(this, WWorkshopActivity.class);
+                startActivity(workshopIntent);
+                break;
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/rhythm_n_reps/WWorkshopActivity.java
@@ -0,0 +1,14 @@
+package com.example.rhythm_n_reps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WWorkshopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wworkshop);
+    }
+}
