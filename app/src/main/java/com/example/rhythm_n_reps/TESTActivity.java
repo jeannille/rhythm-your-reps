ebe99551c96d65b46ce51ff51131e99a4de83493
jeannillehici <hicianoj@northeastern.edu>
2021-12-13 01:18:44 -0500

    testActivity successfully handles 2 category spinners and returnes result to WebService

--- app/src/main/java/com/example/rhythm_n_reps/TESTActivity.java
+++ app/src/main/java/com/example/rhythm_n_reps/TESTActivity.java
@@ -18,12 +18,16 @@ class to test connection between user selection and passing to web service
 
 public class TESTActivity extends AppCompatActivity {
 
+    //resulting string values from user selection to parse in url for correct endpoint to web server
     private String selectedTarget;
+    private String selectedBodyPart;
+    private String selectedEquipment;
+
+    //buttons to submit search
     private Button targetButton;
 
-    private static final int request_code = 5;
 
-    String[] bodyParts = new String[]{
+    private String[] bodyParts = new String[]{
             "back",
             "cardio",
             "chest",
@@ -56,6 +60,36 @@ public class TESTActivity extends AppCompatActivity {
             "triceps",
             "upper back"};
 
+    //    /exercises/equipment/{type}
+    private String[] equipment = new String[]{
+            "assisted",
+            "band",
+            "barbell",
+            "body weight",
+            "bosu ball",
+            "cable",
+            "dumbbell",
+            "elliptical machine",
+            "ez barbell",
+            "hammer",
+            "kettlebell",
+            "leverage machine",
+            "medicine ball",
+            "olympic barbell",
+            "resistance band",
+            "roller",
+            "rope",
+            "skierg machine",
+            "sled machine",
+            "smith machine",
+            "stability ball",
+            "stationary bike",
+            "stepmill machine",
+            "tire",
+            "trap bar",
+            "upper body ergometer",
+            "weighted",
+            "wheel roller"};
 
 
     @Override
@@ -63,7 +97,8 @@ public class TESTActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testactivity);
 
-        Spinner spinnerBodyParts = (Spinner) findViewById(R.id.testSpinner);
+
+        Spinner spinnerBodyParts = (Spinner) findViewById(R.id.bodyPart_spinner);
         //equip spinner w/ listener
 //        spinnerBodyParts.setOnItemSelectedListener(this);
         ArrayAdapter adapterB = new ArrayAdapter(this, android.R.layout.simple_spinner_item, bodyParts);
@@ -76,8 +111,31 @@ public class TESTActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> adapterB, View view, int i, long l) {
 
                 //store selcted value, do not do intent, only push intent when target button clicked
-                selectedTarget = adapterB.getItemAtPosition(adapterB.getSelectedItemPosition()).toString();
-                Toast.makeText(TESTActivity.this, "HAVE selected-------" + selectedTarget, Toast.LENGTH_SHORT).show();
+                selectedBodyPart = adapterB.getItemAtPosition(adapterB.getSelectedItemPosition()).toString();
+                Toast.makeText(TESTActivity.this, "HAVE selected BODYPART -------" + selectedBodyPart, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // not applicable
+
+            }
+        });
+
+        //set target Spinner, its adapter and itemClickelistener
+        //Target category Spinner
+        Spinner spinnerTarget = (Spinner) findViewById(R.id.target_spinner);
+        ArrayAdapter adapterT = new ArrayAdapter(this, android.R.layout.simple_spinner_item, target);
+        adapterT.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerTarget.setAdapter(adapterT);
+
+        spinnerTarget.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterT, View view, int i, long l) {
+
+                //store selected value from drop down, specified target adapter view
+                selectedTarget = adapterT.getItemAtPosition(adapterT.getSelectedItemPosition()).toString();
+                Toast.makeText(TESTActivity.this, "HAVE selected TARGET -------" + selectedTarget, Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -93,16 +151,28 @@ public class TESTActivity extends AppCompatActivity {
 
     public void goToService(View view) {
         switch (view.getId()) {
-            case R.id.testGetWebServiceResults:
-                Toast.makeText(TESTActivity.this, "HAVE CLICKED TARGET BUTTON--- and val is " + selectedTarget  , Toast.LENGTH_SHORT).show();
+            case R.id.selectBodyPartSearchButton:
+                Toast.makeText(TESTActivity.this, "HAVE CLICKED BODYPART BUTTON--- and val is " + selectedBodyPart  , Toast.LENGTH_SHORT).show();
 
                 Intent intent = new Intent(this, WebServiceActivity.class);
-                String sendOver = "bodyPart/" + selectedTarget;
+                String sendOver = "bodyPart/" + selectedBodyPart;
                 Log.i("TESTActivity activity have gotten value of drop down", sendOver);
                 intent.putExtra("var", sendOver);
 
                 startActivity(intent);
                 break;
+
+            case R.id.selectTargetSearchButton:
+                Toast.makeText(TESTActivity.this, "HAVE CLICKED TARGET BUTTON--- and val is " + selectedTarget  , Toast.LENGTH_SHORT).show();
+
+                Intent intent2 = new Intent(this, WebServiceActivity.class);
+                String sendOver2 = "target/" + selectedTarget;
+                Log.i("TARGET BUTTON switch case ----- INTENT----- TESTActivity activity have gotten value of drop down", sendOver2);
+                intent2.putExtra("var", sendOver2);
+
+                startActivity(intent2);
+                break;
+
 
         }
     }
